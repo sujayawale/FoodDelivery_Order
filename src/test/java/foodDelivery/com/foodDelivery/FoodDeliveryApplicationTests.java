b8557966ccf2d7035a19c54d587c48ebe433e16f
Sujay Awale <sujayawale125@gmail.com>
2026-05-05 23:08:21 +0530

    Initial push

---
+++ src/test/java/foodDelivery/com/foodDelivery/FoodDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package foodDelivery.com.foodDelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
